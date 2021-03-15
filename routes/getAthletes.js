6eff628c1bdaade3d036a0fa0a88603465d1bd60
cajunintexas.cm@gmail.com <cajunintexas.cm@gmail.com>
2021-03-15 09:38:42 -0500

    worked on bug fixes

--- routes/getAthletes.js
+++ routes/getAthletes.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const db = require("../models");
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
+const { response } = require('express');
 
-router.get('/', (req, res) => {
+
+
+router.get('/', async (req, res) => {
+  const data = await db.Client.findAll();
+
   res.render("athletes_list", {
     locals: {
       error: null,
       title: "Athletes List",
+      athletes: data
     },
     partials: {
       head: "/partials/head_2"
     }
-  });
+  })
 })
 
 module.exports = router
